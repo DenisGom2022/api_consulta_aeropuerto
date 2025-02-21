9ce4388f766bd72cd95bd71194a5fa8a7e74ae1c
Denis Gomez <denis.gomez.pineda@gmail.com>
2025-02-20 21:59:56 -0600

    second commit

--- dist/coneccion.js
+++ dist/coneccion.js
@@ -1,5 +1,11 @@
-import mysql from 'mysql2/promise';
-export const pool = mysql.createPool({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
+var promise_1 = __importDefault(require("mysql2/promise"));
+exports.pool = promise_1.default.createPool({
     host: 'mysql-aerolineaia2025.alwaysdata.net',
     user: '400640',
     database: 'aerolineaia2025_aerolinea',
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,45 +8,118 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import express from "express";
-import dotenv from "dotenv";
-import { pool } from "./coneccion";
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var coneccion_1 = require("./coneccion");
 // configures dotenv to work in your application
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT;
-app.get("/", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
-    const aerolineas = yield getAllAerolineas();
-    const aeropuertos = yield getAllAeropuertos();
-    const vuelos = yield getAllVuelos();
-    const pasajeros = yield getAllPasajeros();
-    const data = {
-        aerolineas,
-        aeropuertos,
-        vuelos,
-        pasajeros
-    };
-    response.send(data);
-}));
-const getAllAerolineas = () => __awaiter(void 0, void 0, void 0, function* () {
-    const [data, metadata] = yield pool.query("select * from aerolineas");
-    return data;
-});
-const getAllAeropuertos = () => __awaiter(void 0, void 0, void 0, function* () {
-    const [data, metadata] = yield pool.query("select * from aeropuertos");
-    return data;
-});
-const getAllVuelos = () => __awaiter(void 0, void 0, void 0, function* () {
-    const [data, metadata] = yield pool.query("select * from vuelos");
-    return data;
-});
-const getAllPasajeros = () => __awaiter(void 0, void 0, void 0, function* () {
-    const [data, metadata] = yield pool.query("select * from pasajeros");
-    return data;
-});
-app.listen(PORT, () => {
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+var PORT = process.env.PORT;
+app.get("/", function (request, response) { return __awaiter(void 0, void 0, void 0, function () {
+    var aerolineas, aeropuertos, vuelos, pasajeros, data;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, getAllAerolineas()];
+            case 1:
+                aerolineas = _a.sent();
+                return [4 /*yield*/, getAllAeropuertos()];
+            case 2:
+                aeropuertos = _a.sent();
+                return [4 /*yield*/, getAllVuelos()];
+            case 3:
+                vuelos = _a.sent();
+                return [4 /*yield*/, getAllPasajeros()];
+            case 4:
+                pasajeros = _a.sent();
+                data = {
+                    aerolineas: aerolineas,
+                    aeropuertos: aeropuertos,
+                    vuelos: vuelos,
+                    pasajeros: pasajeros
+                };
+                response.send(data);
+                return [2 /*return*/];
+        }
+    });
+}); });
+var getAllAerolineas = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, data, metadata;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0: return [4 /*yield*/, coneccion_1.pool.query("select * from aerolineas")];
+            case 1:
+                _a = _b.sent(), data = _a[0], metadata = _a[1];
+                return [2 /*return*/, data];
+        }
+    });
+}); };
+var getAllAeropuertos = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, data, metadata;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0: return [4 /*yield*/, coneccion_1.pool.query("select * from aeropuertos")];
+            case 1:
+                _a = _b.sent(), data = _a[0], metadata = _a[1];
+                return [2 /*return*/, data];
+        }
+    });
+}); };
+var getAllVuelos = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, data, metadata;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0: return [4 /*yield*/, coneccion_1.pool.query("select * from vuelos")];
+            case 1:
+                _a = _b.sent(), data = _a[0], metadata = _a[1];
+                return [2 /*return*/, data];
+        }
+    });
+}); };
+var getAllPasajeros = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, data, metadata;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0: return [4 /*yield*/, coneccion_1.pool.query("select * from pasajeros")];
+            case 1:
+                _a = _b.sent(), data = _a[0], metadata = _a[1];
+                return [2 /*return*/, data];
+        }
+    });
+}); };
+app.listen(PORT, function () {
     console.log("Server running at PORT: ", PORT);
-}).on("error", (error) => {
+}).on("error", function (error) {
     // gracefully handle error
     throw new Error(error.message);
 });
